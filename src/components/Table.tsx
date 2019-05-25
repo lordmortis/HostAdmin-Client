2359ab4f726cebd057bd4d9fc12c449e3556912a
Brendan Ragan <lordmortis@gmail.com>
2019-05-25 16:06:42 +0800

    actually rendering a table with some data in it

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,13 @@
 import React, {ReactNode} from "react";
 
+
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 
 interface Column {
     field: string;
@@ -16,10 +18,14 @@ interface Column {
 
 interface Props {
     columns: Array<Column>
+    data?: Array<any>
+    fetchFunc?: (start: number, limit: number) => Array<any>
+    total?: number
 }
 
-
 interface IState {
+    page: number,
+    entriesPerPage: number,
 }
 
 export default class CustomTable extends React.Component<Props, IState> {
@@ -32,26 +38,81 @@ export default class CustomTable extends React.Component<Props, IState> {
         }
     }
 
-    private renderHeader() {
+    private getRenderData():Array<object> {
+        const limit = this.state.entriesPerPage;
+        const startRow = this.state.page * limit;
+
+        if (this.props.fetchFunc != null) {
+            return this.props.fetchFunc(startRow, limit);
+        } else if (this.props.data != null) {
+            return this.props.data.slice(startRow, startRow + limit);
+        }
+
+        return [];
+    }
+
+    private renderHeader():ReactNode {
         const {columns} = this.props;
 
         return (
             <TableHead>
-                {columns.map(column => {
-                    return (
-                        <TableCell key={column.field}>
-                            {column.headerFunc !== undefined ? column.headerFunc() : column.header}
-                        </TableCell>
-                    );
-                })}
+                <TableRow>
+                    {columns.map(column => {
+                        return (
+                            <TableCell key={column.field}>
+                                {column.headerFunc !== undefined ? column.headerFunc() : column.header}
+                            </TableCell>
+                        );
+                    })}
+                </TableRow>
             </TableHead>
         );
+    }
+
+    private renderBody():ReactNode {
+        if (this.props.data == null && this.props.fetchFunc == null) {
+            return (
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <Typography children={"No Data or Datafunc provided"}/>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            );
+        }
+
+        let rowData = this.getRenderData();
+
+        return(
+            <TableBody>
+                {rowData.map(value => { return this.renderRow(value); })}
+            </TableBody>
+        );
+    }
+
+    private renderRow(row: any):ReactNode {
+        // @ts-ignore
+        const key = row.id != null ? row.id : "unknown_key";
+        const cells = this.props.columns.map(column => {
+            // @ts-ignore
+            return (
+               <TableCell key={column.field}>
+                   {column.cellFunc != null ? column.cellFunc(row) : row[column.field]}
+               </TableCell>
+           )
+        });
+
+        return (
+            <TableRow key={key} children={cells}/>
+        )
     }
 
     render() {
         return (
             <Table>
                 {this.renderHeader()}
+                {this.renderBody()}
             </Table>
         )
     }
--- src/pages/Domains.tsx
+++ src/pages/Domains.tsx
@@ -19,9 +19,49 @@ type AllProps = PropsFromState & PropsFromDispatch
 interface IState {
 }
 
+const data = [
+    {id: 0, name: "test0.com"},
+    {id: 1, name: "test1.com"},
+    {id: 2, name: "test2.com"},
+    {id: 3, name: "test3.com"},
+    {id: 4, name: "test4.com"},
+    {id: 5, name: "test5.com"},
+    {id: 6, name: "test6.com"},
+    {id: 7, name: "test7.com"},
+    {id: 8, name: "test8.com"},
+    {id: 9, name: "test9.com"},
+    {id: 10, name: "test10.com"},
+    {id: 11, name: "test11.com"},
+    {id: 12, name: "test12.com"},
+    {id: 13, name: "test13.com"},
+    {id: 14, name: "test14.com"},
+    {id: 15, name: "test15.com"},
+    {id: 16, name: "test16.com"},
+    {id: 17, name: "test17.com"},
+    {id: 18, name: "test18.com"},
+    {id: 19, name: "test19.com"},
+    {id: 20, name: "test20.com"},
+    {id: 21, name: "test21.com"},
+    {id: 22, name: "test22.com"},
+    {id: 23, name: "test23.com"},
+    {id: 24, name: "test24.com"},
+    {id: 25, name: "test25.com"},
+    {id: 26, name: "test26.com"},
+    {id: 27, name: "test27.com"},
+    {id: 28, name: "test28.com"},
+    {id: 29, name: "test29.com"},
+];
+
 const columns = [
     {
         field: "name", header: "Domain Name"
+    },
+    {
+        field: "actions", header: "Actions and a long long thing here", cellFunc: (data:any) => {
+            return (
+                <Typography>Ya mama {data.name}</Typography>
+            )
+        }
     }
 ];
 
@@ -30,8 +70,7 @@ class Domains extends React.Component<AllProps, IState> {
         return (
             <Paper id="domains">
                 <Typography variant="title">Domains</Typography>
-                <Table columns={columns}>
-
+                <Table columns={columns} data={data}>
                 </Table>
             </Paper>
         );
